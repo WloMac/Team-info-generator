3f2cf8248b0acf42f8ad199decd53aaf7ad33891
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2023-02-28 12:47:53 +0000

    adding classes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,55 +13,150 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-let {name, id, email, } = await inquirer
-.prompt([
-    //Manager how to specify is manager?
-    {
-        type: 'input',
-        name: 'name',
-        message: "What is your Name?",
-        default(){
-            return 'Your name';
-        }
-      },
-      {
-        type: 'input',
-        name: 'id',
-        message: "What is your Employee ID?",
-        default(){
-            return 'Your EID';
-        }
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: "What is your e-mail?",
-        default(){
-            return 'Youremail@somewhere.com';
-        }
-      },
-      {
-        type: 'list',
-        name: 'size',
-        message: 'What size do you need?',
-        choices: ['Add an engineer', 'Ad an intern', 'Finish building the team'],
-        filter(val) {
-          return val.toLowerCase();
-        },
-      },
+let team = [];
+// Manager's detail
+let managerDetails = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "Please provide manager name",
+    default(){
+        return 'NoName';
+    }
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "Please provide your ID",
+    default(){
+        return 'NoID';
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Please provide your email",
+    default(){
+        return 'NoEmail';
+    }
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: "Please provide your Office Number",
+    default(){
+        return 'NoNum';
+    }
+  },
+
+]
+// Engineer's detail
+
+let engineerDetails = [{
+  type: 'input',
+  name: 'name',
+  message: "Please provide engineer name",
+  default(){
+      return 'NoName';
+  }
+},
+{
+  type: 'input',
+  name: 'id',
+  message: "Please provide ID",
+  default(){
+      return 'NoID';
+  }
+},
+{
+  type: 'input',
+  name: 'email',
+  message: "Please provide email",
+  default(){
+      return 'NoEmail';
+  }
+},
+{
+  type: 'input',
+  name: 'github',
+  message: "Please provide Github username",
+  default(){
+      return 'NoNum';
+  }
+}]
+// Intern's detail
+let internDetails = [
+  {
+    type: 'input',
+    name: 'name',
+    message: "Please provide intern's name",
+    default(){
+        return 'NoName';
+    }
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "Please provide ID",
+    default(){
+        return 'NoID';
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Please provide email",
+    default(){
+        return 'NoEmail';
+    }
+  },
+  {
+    type: 'input',
+    name: 'school',
+    message: "Please provide school",
+    default(){
+        return 'NoNum';
+    }
+  }]
+
+teamGenerator()
+async function teamGenerator() {  
+    let {name, id, email, officeNumber, github, school } = await inquirer
+    // type of employee selection 
+    // if manager go to managerGenerator?
+    // if engineer go to engineerGenerator?
+    // if intern go to internGenerator? 
+
+    // managerGenerator(){ is prompts for manager as below?}
+  
+    .prompt(managerDetails);
+
+    // then?
+    await inquirer    
+    .prompt(engineerDetails);
+  
+
+ 
+
+  
+  
+
+
+  
+  team.push(new Manager (name, id, email, officeNumber ));
+  team.push(new Engineer (name, id, email, github ));
+  // team.push(new Intern (name, id, email, school ));
+
+  let htmlDoc = render(team)
+
+  await fs.writeFile(outputPath, htmlDoc);
+
+  
+}
 
 
 
-])
 
 
+//ask for other members of team
 
-
-
-let developmentTeamContent = `
-
-
-
-`
-
-fs.writeFile("index.html", developmentTeamContent);
